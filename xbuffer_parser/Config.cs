aa762700e1107d6f3f7402dc0178757585318879
lisiyu <576603306@qq.com>
2017-11-17 17:18:26 +0800

    删除多余的库.添加本地配置文件.支持自定义的东西.为添加Typescript版本做准备

---
+++ xbuffer_parser/Config.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+namespace xbuffer
+{
+    class Config
+    {
+        private const string path_config = "config.ini";            // 配置目录
+
+        public static string path_template = "templates";           // 模板目录
+        public static string path_output = "output";                // 输出目录
+        public static bool package = false;                         // 打包到一起
+        public static string suffix = "cs";                         // 文件后缀
+
+        static Config()
+        {
+            load();
+        }
+
+        public static void load()
+        {
+            if (!File.Exists(path_config))
+                return;
+
+            var lines = File.ReadAllLines(path_config);
+            foreach (var line in lines)
+            {
+                var strs = line.Split('=');
+                if (strs.Length < 2)
+                    continue;
+
+                var key = strs[0];
+                var value = strs[1];
+
+                if (key == "path_template") path_template = value;
+                else if (key == "path_output") path_output = value;
+                else if (key == "package") package = bool.Parse(value);
+                else if (key == "suffix") suffix = value;
+            }
+        }
+    }
+}
--- xbuffer_parser/Program.cs
+++ xbuffer_parser/Program.cs
@@ -17,21 +17,35 @@ namespace xbuffer
             var proto = File.ReadAllText(args[0]);
             var proto_classs = new Proto(proto).class_protos;
 
-            var templates = new DirectoryInfo("templates").GetFiles();
+            var templates = new DirectoryInfo(Config.path_template).GetFiles();
             foreach (var template in templates)
             {
-                var template_str = File.ReadAllText("templates/" + template.Name);
+                var template_str = File.ReadAllText(Config.path_template + "/" + template.Name);
                 var template_name = Path.GetFileNameWithoutExtension(template.Name);
+                var isBuffer = template_name.Contains("buffer");
 
                 Console.WriteLine("开始解析模板 " + template_name + " ...");
 
-                Directory.CreateDirectory("output_" + template_name);
-                var isBuffer = template_name.Contains("buffer");
+                if (Config.package)
+                    Directory.CreateDirectory(Config.path_output);
+                else
+                    Directory.CreateDirectory(Config.path_output + "/" + template_name);
+                var output = "";
                 foreach (var proto_class in proto_classs)
                 {
-                    var output = Parser.parse(proto_class, template_str);
-                    File.WriteAllText("output_" + template_name + "/" + proto_class.Class_Name + (isBuffer ? "Buffer" : "") + ".cs", output);
+                    if(Config.package)
+                    {
+                        output += Parser.parse(proto_class, template_str);
+                        output += "\n\n";
+                    }
+                    else
+                    {
+                        output = Parser.parse(proto_class, template_str);
+                        File.WriteAllText(Config.path_output + "/" + template_name + "/" + proto_class.Class_Name + (isBuffer ? "Buffer" : "") + "." + Config.suffix, output);
+                    }
                 }
+                if (Config.package)
+                    File.WriteAllText(Config.path_output + "/" + template_name + "." + Config.suffix, output);
             }
 
             Console.WriteLine("生成完毕");
